3d8040fb4ee457c7c0d585f0facb5e008e155810
edugallegosd <86889539+edugallegosd@users.noreply.github.com>
2022-02-17 08:22:19 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace prueba2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char caracter;
+
+            Console.WriteLine("Ingrese un caracter: ");
+            caracter = char.Parse(Console.ReadLine());
+
+            if (caracter >= 'a' && caracter <= 'z') {
+                Console.WriteLine("La letra " + caracter + " es miniscula");
+            }else if(caracter >= 'A' && caracter <= 'Z') {
+                Console.WriteLine("La letra " + caracter + " es mayuscula");
+            }else{
+                Console.WriteLine("Error");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
